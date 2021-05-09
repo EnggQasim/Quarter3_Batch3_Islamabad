cbea5a55bfc02656d47b76305efe957d0dca3b5b
Muhammad Qasim <m.qasim077@gmail.com>
2021-05-09 11:09:51 +0500

    VAE and GAN

--- flask2/model_prediction/app.py
+++ flask2/model_prediction/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from tensorflow import keras
-m = keras.models.load_model('../abc.h5')
+import tensorflow as tf
+import numpy as np
+m = tf.keras.models.load_model('abc.h5')
 
 
 app = Flask(__name__)
@@ -23,6 +25,12 @@ def info():
     if request.method == "GET":
        pass
     elif request.method == "POST":
-        a = m.predict([[request.form['height']]])
-        return "Data is showing with POST method {} Now prediction is: {}".format(request.form['height'],a)
+        print(m.summary())
+        print(request.form['height'])
+        input1 = np.int64([[request.form['height']]])
+        print("input",input1, type(input1))
+        # a = m.predict([[200]])
+        a = m.predict(input1)
+        print("Prediction...",a)
+        return "Data is showing with POST method {} Now prediction is: {}".format(input1,a)
 app.run(debug=True)    
